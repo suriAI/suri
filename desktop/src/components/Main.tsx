8141a2e93e1c12f18a4169c04e1488d8a21aa448
johnraivenolazo <johnraivenolazo@gmail.com>
2025-09-16 00:11:12 +0800

    Add temporary height to main container to fit the content

--- desktop/src/components/Main.tsx
+++ desktop/src/components/Main.tsx
@@ -1301,7 +1301,7 @@ export default function LiveCameraRecognition({ onMenuSelect }: LiveCameraRecogn
   }, [stopCamera]);
 
   return (
-    <div className= "text-white flex flex-col h-[95vh]">
+    <div className= "text-white flex flex-col h-[93.65216vh]">
 
       {/* Main Content */}
       <div className="flex-1 flex min-h-0">
