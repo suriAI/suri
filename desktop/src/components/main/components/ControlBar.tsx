45855b6aaa4e1067cb88c559302d69962844954b
johnraivenolazo@gmail.com <johnraivenolazo@gmail.com>
2025-10-22 21:02:53 +0800

    Remove dot indicator in Controlbar

--- desktop/src/components/main/components/ControlBar.tsx
+++ desktop/src/components/main/components/ControlBar.tsx
@@ -51,9 +51,6 @@ export function ControlBar({
               </div>
             </div>
           )}
-          <div className="flex items-center space-x-2">
-            <div className={`w-3 h-3 rounded-full ${isStreaming ? 'bg-green-400' : 'bg-red-400'}`}></div>
-          </div>
         </div>
 
         {/* Start/Stop Button */}
